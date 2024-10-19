9cfa135ced8e1408e36872bf270c327df8bf6590
muhammadBintang-k <bintangmuhammad2304@gmail.com>
2024-10-19 12:44:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        first: "#FFF8E8",
+        second: "#F7EED3",
+        button: "#AAB396",
+        hover: "#674636",
+        add: "#efefef",
+      },
+    },
+  },
+  plugins: [],
+};
